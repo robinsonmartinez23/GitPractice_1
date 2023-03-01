c4308b742db8a6ffcf0ed78976f40d94cd7f75a6
Robinson Martinez Martinez <robinsonmartinez23@hotmail.com>
2023-03-01 17:09:37 -0500

    Part 6 done

---
+++ src/day1/RessetingPractice.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class RessetingPractice {
+    //Part 1 done
+    //Part 2 done
+    //Part 3 done
+    //Part 4 done
+    //Part 5 done
+    //Part 6 done
+    //Mixed reset remove the commit from history but keep the local changes
+    //Did part 4 par 5 and, I am done in one commit
+}
